dd9fd00357bf9d956c3f9e0685d4d264cc3441b2
thisisyomans <tanejamm@gmail.com>
2018-11-28 12:05:53 -0800

    window size, the easy way part 3

--- writer.c
+++ writer.c
@@ -15,6 +15,8 @@
 /*** data ***/
 
 struct editorConfig {
+	int screenrows;
+	int screencols;
 	struct termios orig_termios;
 };
 
@@ -105,9 +107,14 @@ void editorProcessKeypress() {
 
 /*** init ***/
 
+void initEditor() {
+	if(getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main() {
 	enableRawMode();	
-	
+	initEditor();	
+
 	while (1) {
 		editorRefreshScreen();
 		editorProcessKeypress();
